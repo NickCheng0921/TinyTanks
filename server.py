283b2ed534ad2ccbe28ed5b99271af77c5d75b9c
NickCheng0921 <nickshacheng.0921@gmail.com>
2021-10-19 13:00:23 -0500

    User will now update own message list, no redundant messages grabbed

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from datetime import *
+import random 
+import string
 import asyncio
 import websockets
 import sys
@@ -6,23 +8,36 @@ import sys
 # https://limecoda.com/how-to-build-basic-websocket-server-python/
 print("Running Server...")
 server_id = "XSERVERX"
+MAX_MESSAGES = 80
 msg_list = []
 
 async def server(websocket, path):
+    my_msg_id_list = []
     print("User joined")
     # Get received data from websocket
     try:
         while(True):
             data = await websocket.recv()
             id, content, msg = decode_msg(data.decode("utf-8"))
-            msg_list.append( (id, msg) )
 
             if(content == "01"):
+                """
+                #halve server list if it gets too large
+                if(len(msg_list) > MAX_MESSAGES):
+                    msg_list = msg_list[len(msg_list)/2:]
+                #halve our personal list if it gets too large
+                if(len(my_msg_id_list) > MAX_MESSAGES):
+                    my_msg_id_list = my_msg_id_list[len(my_msg_id_list)/2:]
+                """
 
-                for id_l, msg_l in msg_list:
-                    #time = datetime.strptime(datetime.now().strftime("%d/%m/%y %H:%M"), "%d/%m/%y %H:%M")
-                    payload = f"{server_id}02{id_l} > {msg_l}\n"
-                    await websocket.send(payload)
+                msg_id = random_string(8)
+                msg_list.append( (msg_id, id, msg) )
+                for msg_id_l, id_l, msg_l in msg_list:
+                    if(msg_id_l not in my_msg_id_list):
+                        my_msg_id_list.append(msg_id_l)
+                        payload = f"{msg_id_l}02{id_l} > {msg_l}\n"
+                        await websocket.send(payload)
+
     except Exception:
         print("User left")
         return
@@ -37,6 +52,10 @@ def decode_msg(message):
     content = message[10:]
 
     return id, type, content
+
+def random_string(length):
+    #generates a random_string of a given length
+    return ''.join(random.choice(string.ascii_letters) for x in range(length))
 
 # Create websocket server
 start_server = websockets.serve(server, "localhost", 8000)
